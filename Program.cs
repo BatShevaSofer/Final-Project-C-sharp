d08f8eff57fd8f01b1747ad1ab67fcabe7089467
tehila12 <tehilag@darkeychana.com>
2023-01-23 13:15:34 +0200

    23.01.2023 13:15

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("H:\\תכנות שנה ב תשפג\\Shevi Sofer\\פרויקט גמר\\Encryption\\ConsoleApp1\\Encryption.dll", EntryPoint= "testBinaryTree01")]
+static extern void testBinaryTree01();
+
+
+
+testBinaryTree01();
